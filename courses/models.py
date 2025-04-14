c4bd0226089a8830bac4c5eac6f0b4fbc91c8c54
nrao <nrao200423@gmail.com>
2025-04-14 15:42:50 -0700

    added course model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# course model rep. online course
+class Course(models.Model):
+    # title of course
+    title = models.CharField(max_length = 150)
+    # short description
+    description = models.TextField(blank = True)
+    # category (programming, linear alg., etc)
+    category = models.CharField(max_length = 100, blank = True)
+    # date when course published
+    publication_date = models.DateField(null = True, blank = True)
+    
+    def __str__(self):
+        # ret. title as str. rep.
+        return self.title
